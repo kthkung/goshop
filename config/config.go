0a2a92ddbafc353a137cd27ef564c3e05c4cb589
Nilloc\_Nilloc <kong@applicationdd.com>
2022-05-29 01:52:51 +0700

    add db_pass

--- config/config.go
+++ config/config.go
@@ -22,11 +22,12 @@ func DatabaseConnection() *gorm.DB {
 	}
 
 	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Printf("Error: %s", err.Error())
